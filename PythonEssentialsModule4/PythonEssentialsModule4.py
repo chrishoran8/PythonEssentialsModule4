4b2da949c468dcdaf81c1ea9d48a74ec0d4e21c5
chrishoran8 <horan_chris@hotmail.co.uk>
2021-05-24 22:20:28 +0100

    days, months, leap years done to 4.3.1.8

--- PythonEssentialsModule4/PythonEssentialsModule4.py
+++ PythonEssentialsModule4/PythonEssentialsModule4.py
@@ -1,3 +1,15 @@
+#from an earlier lab
+def strange_list_fun(n):
+    strange_list = []    
+    for i in range(0, n):
+        strange_list.insert(0, i)    
+    return strange_list
+
+#print(strange_list_fun(5))
+
+
+#labs for 4.3.1.6 - 4.3.1.8
+
 def is_year_leap(year):
     retValue = None
     if year % 4 == 0 :
@@ -26,7 +38,21 @@ def day_of_year(year, month, day):
         totalDays = totalDays + days_in_month(year,i)
     return totalDays
 
-print(is_year_leap(3))
-print(days_in_month(1900,2))
+#print(is_year_leap(3))
+#print(days_in_month(1900,2))
+#print(day_of_year(1999, 12, 31))
 
-print(day_of_year(1999, 12, 31))
+
+
+#4.3.1.9 LAB
+def is_prime(num):
+    for i in range(3,num-1):
+        if num%i == 0:
+            return False
+    return True
+            
+#test case    
+for i in range(1, 20):
+	if is_prime(i + 1):
+			print(i + 1, end=" ")
+print()
