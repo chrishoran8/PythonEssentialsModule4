86cff775970000baab2774831a98312cd16c3e3d
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-25 10:27:32 +0100

    ready

--- PythonEssentialsModule4/PythonEssentialsModule4.py
+++ PythonEssentialsModule4/PythonEssentialsModule4.py
@@ -1,42 +1,55 @@
-#from an earlier lab
-def strange_list_fun(n):
-    strange_list = []    
-    for i in range(0, n):
-        strange_list.insert(0, i)    
-    return strange_list
-
-#print(strange_list_fun(5))
-
 
 #labs for 4.3.1.6 - 4.3.1.8
 
 def is_year_leap(year):
-    retValue = None
-    if year % 4 == 0 :
-        retValue = True
+    #return true if leap year
+    modByFour = year%4
+    modByHundred = year % 100
+    modByfourHundred = year % 400
+    if year <1582:
+        #print("Not within greg cal")
+        return False
+    elif modByFour != 0: # not divisible by four? 
+        #print("common year") #common year
+        return False
+    elif modByHundred != 0: # not divisible by 100? 
+        #print("leap year") # leap year
+        return True
+    elif modByfourHundred != 0: #not divisible by 400?
+        #print("common year")
+        return False
     else:
-        retValue = False
-        
-    if year % 100 ==0:
-        if year % 400 == 0:
-            retValue = True
-        else: 
-            retValue = False
-    return retValue
+        #print("leap year")
+        return True
+    
+    #just in case
+    return False
 
 
 def days_in_month(year, month):
-    days=[31,28,31,30,31,30,31,31,30,31,30,31]
-    retValue = days[month-1]
-    if is_year_leap(year) and month==2:
+    retValue = 0
+    days = [31,28,31,30,31,30,31,31,30,31,30,31]
+    
+    retValue = days[month-1] #that will change normal month index, to our month index
+    if is_year_leap(year) and month ==2:
         retValue = 29
     return retValue
 
 def day_of_year(year, month, day):
-    totalDays = 0
-    for i in range (month):
-        totalDays = totalDays + days_in_month(year,i)
-    return totalDays
+    if(month>12):
+        return None
+    #if the days are greater than the month allows
+    if day > days_in_month(year,month):
+        return None
+    
+        
+    totalDays = 0 #this tracks the total days added so far
+    for i in range(month-1):
+        totalDays = totalDays + days_in_month(year,i+1)
+    return totalDays + day
+
+print(day_of_year(2001, 2, 29))
+
 
 #print(is_year_leap(3))
 #print(days_in_month(1900,2))
@@ -57,22 +70,4 @@ for i in range(1, 200):
 			print(i + 1, end=" ")
 print()
 
-#4.3.1.10
-def liters_100km_to_miles_gallon(liters):
-    kmPerLitre = 100/liters
-    kmPerGallon = kmPerLitre * 3.785411784
-    milesPerGallon = kmPerGallon / 1.609344
-    return milesPerGallon
 
-def miles_gallon_to_liters_100km(miles):
-    kmPerGallon = miles * 1.609344
-    kmPerLitre = kmPerGallon / 3.785411784
-    liters = 100/kmPerLitre
-    return liters
-
-print(liters_100km_to_miles_gallon(3.9))
-print(liters_100km_to_miles_gallon(7.5))
-print(liters_100km_to_miles_gallon(10.))
-print(miles_gallon_to_liters_100km(60.3))
-print(miles_gallon_to_liters_100km(31.4))
-print(miles_gallon_to_liters_100km(23.5))
